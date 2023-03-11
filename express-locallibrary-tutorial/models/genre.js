0a385ae88d3b9d7369fb4f307f452f64ce03dcf3
Josh Hancock <livingicon@gmail.com>
2023-03-11 17:14:38 -0500

    Added other schemas other than only rough draft for genre.js

---
+++ express-locallibrary-tutorial/models/genre.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const GenreSchema = new Schema({
+  genre: [{ type: Schema.Types.ObjectId, ref: "Genre" }],
+});
+
+// Virtual for book's URL
+BookSchema.virtual("url").get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return `/catalog/book/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model("Book", BookSchema);
